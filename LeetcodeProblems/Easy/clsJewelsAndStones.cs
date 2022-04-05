e746bb72a75de6f8e451f67ac22c35791b4dd03d
Arpit S <arpittempacc@gmail.com>
2022-04-05 08:00:40 -0400

    771. Jewels and Stones

---
+++ LeetcodeProblems/Easy/clsJewelsAndStones.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*771. Jewels and Stones
+     * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+        Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ 
+        Example 1:
+        Input: jewels = "aA", stones = "aAAbbbb"
+        Output: 3
+
+        Example 2:
+        Input: jewels = "z", stones = "ZZ"
+        Output: 0
+ 
+        Constraints:
+        1 <= jewels.length, stones.length <= 50
+        jewels and stones consist of only English letters.
+        All the characters of jewels are unique.
+    */
+    public class clsJewelsAndStones
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int output = 0;
+            HashSet<char> hs = new HashSet<char>();
+            for (int i = 0; i < jewels.Length; i++)
+            {
+                hs.Add(jewels[i]);
+            }
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (hs.Contains(stones[i]))
+                    output++;
+            }
+            return output;
+        }
+    }
+}
