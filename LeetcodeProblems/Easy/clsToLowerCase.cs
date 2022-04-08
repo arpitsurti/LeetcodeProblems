268215e41090744eec4f0b27f4391b4707dd358e
Arpit S <arpittempacc@gmail.com>
2022-04-08 11:06:47 -0400

    709. To Lower Case

---
+++ LeetcodeProblems/Easy/clsToLowerCase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*709. To Lower Case
+     * Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+        Example 1:
+        Input: s = "Hello"
+        Output: "hello"
+
+        Example 2:
+        Input: s = "here"
+        Output: "here"
+
+        Example 3:
+        Input: s = "LOVELY"
+        Output: "lovely"
+
+        Constraints:
+        1 <= s.length <= 100
+        s consists of printable ASCII characters.
+    */
+    public class clsToLowerCase
+    {
+        public string ToLowerCase(string s)
+        {
+            char[] chArr = s.ToCharArray();
+            for (int i = 0; i < chArr.Count(); i++)
+            {
+                if (chArr[i] >= 'A' && chArr[i] <= 'Z')
+                    chArr[i] = (char)(chArr[i] + 32);
+            }
+            return string.Join("", chArr);
+        }
+    }
+}
--- LeetcodeProblems/Program.cs
+++ LeetcodeProblems/Program.cs
@@ -11,6 +11,18 @@ namespace LeetcodeProblems
     {
         static void Main(string[] args)
         {
+            ToLowerCase("Hello@[]");
+        }
+
+        public static string ToLowerCase(string s)
+        {
+            char[] chArr = s.ToCharArray();
+            for (int i = 0; i < chArr.Count(); i++)
+            {
+                if (chArr[i] >= 65 && chArr[i] < 91)
+                    chArr[i] = (char)((int)chArr[i] + 32);
+            }
+            return string.Join("", chArr);
         }
     }
 }
