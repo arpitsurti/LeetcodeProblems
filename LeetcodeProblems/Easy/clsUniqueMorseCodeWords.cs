66aa358d409f9ce7a3882fbcd085794ebddf8c59
Arpit S <arpittempacc@gmail.com>
2022-04-08 14:45:22 -0400

    804. Unique Morse Code Words

---
+++ LeetcodeProblems/Easy/clsUniqueMorseCodeWords.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*804. Unique Morse Code Words
+     * International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+        'a' maps to ".-",
+        'b' maps to "-...",
+        'c' maps to "-.-.", and so on.
+        For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+        [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+        For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+        Return the number of different transformations among all words we have.
+
+        Example 1:
+        Input: words = ["gin","zen","gig","msg"]
+        Output: 2
+        Explanation: The transformation of each word is:
+        "gin" -> "--...-."
+        "zen" -> "--...-."
+        "gig" -> "--...--."
+        "msg" -> "--...--."
+        There are 2 different transformations: "--...-." and "--...--.".
+
+        Example 2:
+        Input: words = ["a"]
+        Output: 1
+
+        Constraints:
+        1 <= words.length <= 100
+        1 <= words[i].length <= 12
+        words[i] consists of lowercase English letters.
+    */
+    public class clsUniqueMorseCodeWords
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] sArr = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            HashSet<string> hs = new HashSet<string>();
+            foreach (string item in words)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (char ch in item)
+                {
+                    sb.Append(sArr[ch - 'a']);
+                }
+                hs.Add(sb.ToString());
+            }
+            return hs.Count();
+        }
+    }
+}
