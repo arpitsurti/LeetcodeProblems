37764888458ceb0722fed610147403a4bec83ccb
Arpit S <arpittempacc@gmail.com>
2022-04-08 10:18:11 -0400

    2103. Rings and Rods

--- LeetcodeProblems/Program.cs
+++ LeetcodeProblems/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetcodeProblems.Easy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace LeetcodeProblems
     {
         static void Main(string[] args)
         {
+            clsRingsAndRods obj = new clsRingsAndRods();
+            obj.CountPoints("B0B6G0R6R0R6G9");
         }
     }
 }
