de39ce45127c0070aa3eea2d7bd4104fdc7434d8
Arpit S <arpittempacc@gmail.com>
2022-04-12 18:20:47 -0400

    226. Invert Binary Tree

---
+++ LeetcodeProblems/Easy/clsInvertBinaryTree.cs
@@ -0,0 +1,44 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*226. Invert Binary Tree
+     * Given the root of a binary tree, invert the tree, and return its root.
+
+        Example 1:
+        Input: root = [4,2,7,1,3,6,9]
+        Output: [4,7,2,9,6,3,1]
+
+        Example 2:
+        Input: root = [2,1,3]
+        Output: [2,3,1]
+
+        Example 3:
+        Input: root = []
+        Output: []
+
+        Constraints:
+        The number of nodes in the tree is in the range [0, 100].
+        -100 <= Node.val <= 100
+    */
+    public class clsInvertBinaryTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+            TreeNode left = InvertTree(root.left);
+            TreeNode right = InvertTree(root.right);
+
+            //swap
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+    }
+}
