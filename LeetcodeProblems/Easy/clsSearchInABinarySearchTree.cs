282c50efb648b951066cc6fbec2a6dda6f4e258f
Arpit S <arpittempacc@gmail.com>
2022-04-11 22:54:35 -0400

    700. Search in a Binary Search Tree

---
+++ LeetcodeProblems/Easy/clsSearchInABinarySearchTree.cs
@@ -0,0 +1,39 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*700. Search in a Binary Search Tree
+     * You are given the root of a binary search tree (BST) and an integer val.
+        Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+        Example 1:
+        Input: root = [4,2,7,1,3], val = 2
+        Output: [2,1,3]
+
+        Example 2:
+        Input: root = [4,2,7,1,3], val = 5
+        Output: []
+ 
+        Constraints:
+        The number of nodes in the tree is in the range [1, 5000].
+        1 <= Node.val <= 107
+        root is a binary search tree.
+        1 <= val <= 107
+    */
+    public class clsSearchInABinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return null;
+            if (root.val == val)
+                return root;
+            return SearchBST(root.left, val) ?? SearchBST(root.right, val);
+        }
+    }
+}
