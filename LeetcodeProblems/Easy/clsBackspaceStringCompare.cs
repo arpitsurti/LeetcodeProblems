165ca9d231f8313e4ec6f19000be4880ed817692
Arpit S <arpittempacc@gmail.com>
2022-04-15 12:20:42 -0400

    844. Backspace String Compare (Approach 1)

---
+++ LeetcodeProblems/Easy/clsBackspaceStringCompare.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*844. Backspace String Compare
+     * Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+        Note that after backspacing an empty text, the text will continue empty.
+
+        Example 1:
+        Input: s = "ab#c", t = "ad#c"
+        Output: true
+        Explanation: Both s and t become "ac".
+
+        Example 2:
+        Input: s = "ab##", t = "c#d#"
+        Output: true
+        Explanation: Both s and t become "".
+
+        Example 3:
+        Input: s = "a#c", t = "b"
+        Output: false
+        Explanation: s becomes "c" while t becomes "b".
+ 
+        Constraints:
+        1 <= s.length, t.length <= 200
+        s and t only contain lowercase letters and '#' characters.
+ 
+        Follow up: Can you solve it in O(n) time and O(1) space?
+    */
+    public class clsBackspaceStringCompare
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            Stack<char> st1 = removeBackSpace(s);
+            Stack<char> st2 = removeBackSpace(t);
+            if (st1.Count() != st2.Count())
+                return false;
+            while (st1.Count() != 0 && st2.Count() != 0)
+            {
+                if (st1.Pop() != st2.Pop())
+                    return false;
+            }
+            return true;
+        }
+
+        private Stack<char> removeBackSpace(string input)
+        {
+            Stack<char> st = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (st.Count() == 0 && input[i] == '#')
+                    continue;
+                if (input[i] == '#' && st.Count() != 0)
+                    st.Pop();
+                else
+                    st.Push(input[i]);
+            }
+            return st;
+        }
+    }
+}
