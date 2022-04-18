1b6bb5bc45a7d44eafe4e5f2294eabf65d53ed44
Arpit S <arpittempacc@gmail.com>
2022-04-17 20:57:23 -0400

    1290. Convert Binary Number in a Linked List to Integer

---
+++ LeetcodeProblems/Easy/clsConvertBinaryNumberInALinkedListToInteger.cs
@@ -0,0 +1,41 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*1290. Convert Binary Number in a Linked List to Integer
+     * Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+        Return the decimal value of the number in the linked list.
+
+        Example 1:
+        Input: head = [1,0,1]
+        Output: 5
+        Explanation: (101) in base 2 = (5) in base 10
+
+        Example 2:
+        Input: head = [0]
+        Output: 0
+ 
+        Constraints:
+        The Linked List is not empty.
+        Number of nodes will not exceed 30.
+        Each node's value is either 0 or 1.
+    */
+    public class clsConvertBinaryNumberInALinkedListToInteger
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            int output = head.val;
+            while (head.next != null)
+            {
+                output = (output << 1) + head.next.val;
+                head = head.next;
+            }
+            return output;
+        }
+    }
+}
