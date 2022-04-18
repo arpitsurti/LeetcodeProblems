8de1e98501a6b0cf640da8b40189dc0df46f77a0
Arpit S <arpittempacc@gmail.com>
2022-04-18 10:28:55 -0400

    21. Merge Two Sorted Lists

---
+++ LeetcodeProblems/Easy/clsMergeTwoSortedLists.cs
@@ -0,0 +1,59 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*21. Merge Two Sorted Lists
+     * You are given the heads of two sorted linked lists list1 and list2.
+        Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+        Return the head of the merged linked list.
+
+        Example 1:
+        Input: list1 = [1,2,4], list2 = [1,3,4]
+        Output: [1,1,2,3,4,4]
+
+        Example 2:
+        Input: list1 = [], list2 = []
+        Output: []
+
+        Example 3:
+        Input: list1 = [], list2 = [0]
+        Output: [0]
+ 
+        Constraints:
+        The number of nodes in both lists is in the range [0, 50].
+        -100 <= Node.val <= 100
+        Both list1 and list2 are sorted in non-decreasing order.
+    */
+    public class clsMergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode head = new ListNode(0);
+            ListNode curr = head;
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    curr.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    curr.next = list2;
+                    list2 = list2.next;
+                }
+                curr = curr.next;
+            }
+            if (list1 != null)
+                curr.next = list1;
+            if (list2 != null)
+                curr.next = list2;
+            return head.next;
+        }
+    }
+}
--- LeetcodeProblems/Program.cs
+++ LeetcodeProblems/Program.cs
@@ -1,4 +1,5 @@
-﻿using LeetcodeProblems.Easy;
+﻿using LeetcodeProblems.CommonClass;
+using LeetcodeProblems.Easy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,16 @@ namespace LeetcodeProblems
     {
         static void Main(string[] args)
         {
+            clsMergeTwoSortedLists obj = new clsMergeTwoSortedLists();
+
+            ListNode lst1 = new ListNode(1);
+            lst1.next = new ListNode(2);
+            lst1.next.next = new ListNode(4);
+
+            ListNode lst2 = new ListNode(1);
+            lst2.next = new ListNode(3);
+            lst2.next.next = new ListNode(4);
+            obj.MergeTwoLists(lst1,lst2);
         }
     }
 }
