0f469ed4adc8645503a62ed9fbca99ba8192d488
Arpit S <arpittempacc@gmail.com>
2022-04-25 19:26:04 -0400

    1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ LeetcodeProblems/Easy/clsCheckIfAWordOccursAsAPrefixOfAnyWordInASentence.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+     * Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
+
+        Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+
+        A prefix of a string s is any leading contiguous substring of s.
+
+        Example 1:
+        Input: sentence = "i love eating burger", searchWord = "burg"
+        Output: 4
+        Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+
+        Example 2:
+        Input: sentence = "this problem is an easy problem", searchWord = "pro"
+        Output: 2
+        Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
+
+        Example 3:
+        Input: sentence = "i am tired", searchWord = "you"
+        Output: -1
+        Explanation: "you" is not a prefix of any word in the sentence.
+ 
+        Constraints:
+        1 <= sentence.length <= 100
+        1 <= searchWord.length <= 10
+        sentence consists of lowercase English letters and spaces.
+        searchWord consists of lowercase English letters.
+    */
+    public class clsCheckIfAWordOccursAsAPrefixOfAnyWordInASentence
+    {
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            int output = -1;
+            string[] str = sentence.Split(' ');
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].StartsWith(searchWord))
+                {
+                    output = i + 1;
+                    break;
+                }
+            }
+            return output;
+        }
+    }
+}
