6096322249d284b16a3b39ff038496d5a1bc359b
Arpit S <arpittempacc@gmail.com>
2022-04-25 09:51:03 -0400

    111. Minimum Depth of Binary Tree 101. Symmetric Tree

---
+++ LeetcodeProblems/Easy/clsMinimumDepthOfBinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*111. Minimum Depth of Binary Tree
+     * Given a binary tree, find its minimum depth.
+        The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+        Note: A leaf is a node with no children.
+
+        Example 1:
+        Input: root = [3,9,20,null,null,15,7]
+        Output: 2
+
+        Example 2:
+        Input: root = [2,null,3,null,4,null,5,null,6]
+        Output: 5
+ 
+        Constraints:
+        The number of nodes in the tree is in the range [0, 105].
+        -1000 <= Node.val <= 1000
+    */
+    class clsMinimumDepthOfBinaryTree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            int left = MinDepth(root.left);
+            int right = MinDepth(root.right);
+            if (left == 0 || right == 0)
+                return Math.Max(left, right) + 1;
+            return Math.Min(left + 1, right + 1);
+        }
+    }
+}
---
+++ LeetcodeProblems/Easy/clsSymmetricTree.cs
@@ -0,0 +1,43 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*101. Symmetric Tree
+     * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+        Example 1:
+        Input: root = [1,2,2,3,4,4,3]
+        Output: true
+
+        Example 2:
+        Input: root = [1,2,2,null,3,null,3]
+        Output: false
+ 
+        Constraints:
+        The number of nodes in the tree is in the range [1, 1000].
+        -100 <= Node.val <= 100
+ 
+        Follow up: Could you solve it both recursively and iteratively?
+    */
+    public class clsSymmetricTree
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            return checkSymm(root.left, root.right);
+        }
+
+        private bool checkSymm(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null || root2 == null)
+                return root1 == root2;
+            if (root1.val != root2.val)
+                return false;
+            return checkSymm(root1.left, root2.right) && checkSymm(root1.right, root2.left);
+        }
+    }
+}
