1848da842c09cae9f6174a1f0cac8dd2d911c224
Arpit S <arpittempacc@gmail.com>
2022-04-25 18:51:50 -0400

    929. Unique Email Addresses

---
+++ LeetcodeProblems/Easy/clsUniqueEmailAddresses.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*929. Unique Email Addresses
+     * Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+
+        For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
+        If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+
+        For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+        If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+
+        For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+        It is possible to use both of these rules at the same time.
+
+        Given an array of strings emails where we send one email to each emails[i], return the number of different addresses that actually receive mails.
+        Example 1:
+        Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+        Output: 2
+        Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+
+        Example 2:
+        Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+        Output: 3
+ 
+        Constraints:
+        1 <= emails.length <= 100
+        1 <= emails[i].length <= 100
+        emails[i] consist of lowercase English letters, '+', '.' and '@'.
+        Each emails[i] contains exactly one '@' character.
+        All local and domain names are non-empty.
+        Local names do not start with a '+' character.
+        Domain names end with the ".com" suffix.
+    */
+    class clsUniqueEmailAddresses
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            HashSet<string> hs = new HashSet<string>();
+            for (int i = 0; i < emails.Length; i++)
+            {
+                string[] str = emails[i].Split('@');
+                StringBuilder sb = new StringBuilder();
+                int j = 0;
+                if (str[0].Contains("+"))
+                {
+                    while (j < str[0].Length)
+                    {
+                        if (str[0][j] == '+')
+                            break;
+                        j++;
+                    }
+                    sb.Append((str[0].Substring(0, j)).Replace(".", "") + "@" + str[1]);
+                }
+                else
+                    sb.Append((str[0].Replace(".", "") + "@" + str[1]));
+                Console.WriteLine(sb.ToString());
+                hs.Add(sb.ToString());
+            }
+            return hs.Count();
+        }
+    }
+}
