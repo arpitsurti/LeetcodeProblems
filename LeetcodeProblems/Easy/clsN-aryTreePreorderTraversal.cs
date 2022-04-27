0e42ac8748153392c901fcc87cd6bac8478b8392
Arpit S <arpittempacc@gmail.com>
2022-04-27 13:25:54 -0400

    589. N-ary Tree Preorder Traversal

---
+++ LeetcodeProblems/Easy/clsN-aryTreePreorderTraversal.cs
@@ -0,0 +1,52 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    public class clsN_aryTreePreorderTraversal
+    {
+        /*589. N-ary Tree Preorder Traversal
+         * Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+        Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+        Example 1:
+        Input: root = [1,null,3,2,4,null,5,6]
+        Output: [1,3,5,6,2,4]
+
+        Example 2:
+        Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+        Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+ 
+        Constraints:
+        The number of nodes in the tree is in the range [0, 104].
+        0 <= Node.val <= 104
+        The height of the n-ary tree is less than or equal to 1000.
+
+        Follow up: Recursive solution is trivial, could you do it iteratively?
+        */
+        public IList<int> Preorder(Node root)
+        {
+            List<int> lstResult = new List<int>();
+            if (root == null)
+                return lstResult;
+            Stack<Node> st = new Stack<Node>();
+            st.Push(root);
+            while (st.Count != 0)
+            {
+                Node curr = st.Pop();
+                lstResult.Add(curr.val);
+                int childrenLength = curr.children.Count - 1;
+                for (int i = childrenLength; i >= 0; i--)
+                {
+                    st.Push(curr.children[i]);
+                }
+            }
+            return lstResult;
+        }
+    }
+}
