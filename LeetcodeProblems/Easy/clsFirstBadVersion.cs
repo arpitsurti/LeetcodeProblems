d43e895c67bdca680e93842d7e84ea7435121376
Arpit S <arpittempacc@gmail.com>
2022-05-10 11:36:51 -0400

    278. First Bad Version

---
+++ LeetcodeProblems/Easy/clsFirstBadVersion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*278. First Bad Version
+     * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+        Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+        You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+        Example 1:
+        Input: n = 5, bad = 4
+        Output: 4
+        Explanation:
+        call isBadVersion(3) -> false
+        call isBadVersion(5) -> true
+        call isBadVersion(4) -> true
+        Then 4 is the first bad version.
+
+        Example 2:
+        Input: n = 1, bad = 1
+        Output: 1
+ 
+        Constraints:
+        1 <= bad <= n <= 231 - 1
+    */
+    public class clsFirstBadVersion
+    {
+        public int FirstBadVersion(int n)
+        {
+            int left = 0;
+            int right = n;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (!IsBadVersion(mid))
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return left;
+        }
+
+        //Below is the dummy method
+        public bool IsBadVersion(int version)
+        {
+            return true;
+        }
+    }
+}
