2b31d04e98265bd76a354abc19e0e78ba356b7c4
Arpit S <arpittempacc@gmail.com>
2022-05-11 18:25:34 -0400

    62. Unique Paths

---
+++ LeetcodeProblems/Medium/clsUniquePaths.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Medium
+{
+    /*62. Unique Paths
+     *** REF ***
+     *https://www.youtube.com/watch?v=6qMFjFC9YSc
+     * There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+        Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+        The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+        Example 1:
+        Input: m = 3, n = 7
+        Output: 28
+
+        Example 2:
+        Input: m = 3, n = 2
+        Output: 3
+        Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+        1. Right -> Down -> Down
+        2. Down -> Down -> Right
+        3. Down -> Right -> Down
+ 
+        Constraints:
+        1 <= m, n <= 100
+    */
+    public class clsUniquePaths
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[,] dp = new int[m, n];
+            // Go all the way on the bottom is one way so updating first column with 1
+            for (int i = 0; i < m; i++)
+            {
+                dp[i, 0] = 1;
+            }
+            // Go all the way on the right is one way so updating first row with 1
+            for (int i = 0; i < n; i++)
+            {
+                dp[0, i] = 1;
+            }
+            // Now complete rest of the dp array
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                }
+            }
+            return dp[m - 1, n - 1];
+        }
+    }
+}
