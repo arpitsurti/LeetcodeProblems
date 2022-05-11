e7e5b77f1afef172aef10b184892a214d5d8853c
Arpit S <arpittempacc@gmail.com>
2022-05-11 13:30:32 -0400

    98. Validate Binary Search Tree 1641. Count Sorted Vowel Strings

---
+++ LeetcodeProblems/Medium/clsCountSortedVowelStrings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Medium
+{
+    /*1641. Count Sorted Vowel Strings
+     *** REF ***
+     *https://leetcode.com/problems/count-sorted-vowel-strings/discuss/918507/Java-DP-O(n)-time-Easy-to-understand
+     * Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
+
+        A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
+
+        Example 1:
+        Input: n = 1
+        Output: 5
+        Explanation: The 5 sorted strings that consist of vowels only are ["a","e","i","o","u"].
+
+        Example 2:
+        Input: n = 2
+        Output: 15
+        Explanation: The 15 sorted strings that consist of vowels only are
+        ["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
+        Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
+
+        Example 3:
+        Input: n = 33
+        Output: 66045
+ 
+        Constraints:
+        1 <= n <= 50 
+    */
+    public class clsCountSortedVowelStrings
+    {
+        public int CountVowelStrings(int n)
+        {
+            int aCount = 1;
+            int eCount = 1;
+            int iCount = 1;
+            int oCount = 1;
+            int uCount = 1;
+            while (n > 1)
+            {
+                aCount = aCount + eCount + iCount + oCount + uCount;
+                eCount = eCount + iCount + oCount + uCount;
+                iCount = iCount + oCount + uCount;
+                oCount = oCount + uCount;
+                uCount = uCount;
+                n--;
+            }
+            return aCount + eCount + iCount + oCount + uCount;
+        }
+    }
+}
---
+++ LeetcodeProblems/Medium/clsValidateBinarySearchTree.cs
@@ -0,0 +1,50 @@
+﻿using LeetcodeProblems.CommonClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Medium
+{
+    /*98. Validate Binary Search Tree
+      Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+        *** REF ***
+        *https://www.youtube.com/watch?v=gGsEVFP0aQo
+        *https://www.youtube.com/watch?v=Z_-h_mpDmeg
+        A valid BST is defined as follows:
+
+        The left subtree of a node contains only nodes with keys less than the node's key.
+        The right subtree of a node contains only nodes with keys greater than the node's key.
+        Both the left and right subtrees must also be binary search trees.
+ 
+        Example 1:
+        Input: root = [2,1,3]
+        Output: true
+    
+        Example 2:
+        Input: root = [5,1,4,null,null,3,6]
+        Output: false
+        Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+        Constraints:
+        The number of nodes in the tree is in the range [1, 104].
+        -231 <= Node.val <= 231 - 1
+    */
+    public class clsValidateBinarySearchTree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return isValid(root, long.MinValue, long.MaxValue);
+        }
+
+        private bool isValid(TreeNode root, long minVal, long maxVal)
+        {
+            if (root == null)
+                return true;
+            if (root.val <= minVal || root.val >= maxVal)
+                return false;
+            return isValid(root.left, minVal, root.val) && isValid(root.right, root.val, maxVal);
+        }
+    }
+}
