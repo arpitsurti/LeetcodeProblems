92a877eceef29255e656d438898c4adf62b3f428
ARPIT <arpittempacc@gmail.com>
2022-05-17 13:08:16 -0400

    33. Search in Rotated Sorted Array

---
+++ LeetcodeProblems/Medium/clsSearchInRotatedSortedArray.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Medium
+{
+    /*33. Search in Rotated Sorted Array
+     *** REF ***
+     *https://www.youtube.com/watch?v=QdVrY3stDD4&t=457s
+     *There is an integer array nums sorted in ascending order (with distinct values).
+
+        Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+        Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+        You must write an algorithm with O(log n) runtime complexity.
+
+        Example 1:
+        Input: nums = [4,5,6,7,0,1,2], target = 0
+        Output: 4
+
+        Example 2:
+        Input: nums = [4,5,6,7,0,1,2], target = 3
+        Output: -1
+
+        Example 3:
+        Input: nums = [1], target = 0
+        Output: -1
+ 
+        Constraints:
+        1 <= nums.length <= 5000
+        -104 <= nums[i] <= 104
+        All values of nums are unique.
+        nums is an ascending array that is possibly rotated.
+        -104 <= target <= 104
+    */
+    public class clsSearchInRotatedSortedArray
+    {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[right])
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+            int start = left;
+            left = 0;
+            right = nums.Length - 1;
+            if (target >= nums[start] && target <= nums[right])
+                left = start;
+            else
+                right = start;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                else if (nums[mid] > target)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
