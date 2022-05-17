05c121ab3ef98571ae117b30be8c177bc9bd221e
ARPIT <arpittempacc@gmail.com>
2022-05-16 23:26:53 -0400

    223. Rectangle Area

--- LeetcodeProblems/Medium/clsFindPeakElement.cs
+++ LeetcodeProblems/Medium/clsFindPeakElement.cs
@@ -19,7 +19,8 @@ namespace LeetcodeProblems.Medium
         Example 1:
         Input: nums = [1,2,3,1]
         Output: 2
-        Explanation: 3 is a peak element and your function should return the index number 2.
+        Explanation: 3 is a peak element and your function should
+    return the index number 2.
     
         Example 2:
         Input: nums = [1,2,1,3,5,6,4]
