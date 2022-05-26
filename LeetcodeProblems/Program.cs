77f0cd0a5da5bddb57f0a0d4d43f4fcd4266916f
ARPIT <arpittempacc@gmail.com>
2022-05-26 17:26:47 -0400

    1004. Max Consecutive Ones III

--- LeetcodeProblems/Program.cs
+++ LeetcodeProblems/Program.cs
@@ -9,9 +9,6 @@ namespace LeetcodeProblems
     {
         static void Main(string[] args)
         {
-            //3,1,3,4,2
-            clsFindTheDuplicateNumber obj = new clsFindTheDuplicateNumber();
-            obj.FindDuplicate(new int[] { 3, 1, 3, 4, 2 });
         }
     }
 }
