8d61d33658dad96cd454683a40b855ce2a3e6dc3
ARPIT <arpittempacc@gmail.com>
2022-05-26 22:39:32 -0400

    200. Number of Islands

---
+++ LeetcodeProblems/Medium/clsNumberOfIslands.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Medium
+{
+    /*200. Number of Islands
+     *** REF ***
+     *https://www.youtube.com/watch?v=U6-X_QOwPcs
+     * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+        An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+        Example 1:
+        Input: grid = [
+          ["1","1","1","1","0"],
+          ["1","1","0","1","0"],
+          ["1","1","0","0","0"],
+          ["0","0","0","0","0"]
+        ]
+        Output: 1
+        
+        Example 2:
+        Input: grid = [
+          ["1","1","0","0","0"],
+          ["1","1","0","0","0"],
+          ["0","0","1","0","0"],
+          ["0","0","0","1","1"]
+        ]
+        Output: 3
+ 
+        Constraints:
+        m == grid.length
+        n == grid[i].length
+        1 <= m, n <= 300
+        grid[i][j] is '0' or '1'.
+    */
+    public class clsNumberOfIslands
+    {
+        public int NumIslands(char[][] grid)
+        {
+            if (grid == null || grid.Length == 0)
+                return 0;
+            int output = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        dfs(grid, i, j);
+                        output++;
+                    }
+                }
+            }
+            return output;
+        }
+
+        private void dfs(char[][] grid, int i, int j)
+        {
+            if (i < 0 || i >= grid.Length || j < 0 || j >= grid[i].Length ||
+              grid[i][j] == '0')
+                return;
+            grid[i][j] = '0';
+            dfs(grid, i + 1, j);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i, j - 1);
+        }
+    }
+}
