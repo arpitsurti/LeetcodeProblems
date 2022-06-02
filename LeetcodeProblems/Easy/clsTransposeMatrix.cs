2a41345f0f3ae8ae48b05d8244192838963e4719
ARPIT <arpittempacc@gmail.com>
2022-06-01 21:59:38 -0400

    867. Transpose Matrix

---
+++ LeetcodeProblems/Easy/clsTransposeMatrix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*867. Transpose Matrix
+     * Given a 2D integer array matrix, return the transpose of matrix.
+
+        The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+        Example 1:
+        Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+        Example 2:
+        Input: matrix = [[1,2,3],[4,5,6]]
+        Output: [[1,4],[2,5],[3,6]]
+ 
+        Constraints:
+        m == matrix.length
+        n == matrix[i].length
+        1 <= m, n <= 1000
+        1 <= m * n <= 105
+        -109 <= matrix[i][j] <= 109
+    */
+    public class clsTransposeMatrix
+    {
+        public int[][] Transpose(int[][] matrix)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            int[][] result = new int[n][];
+            for (int i = 0; i < n; i++)
+                result[i] = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[j][i] = matrix[i][j];
+                }
+            }
+            return result;
+        }
+    }
+}
