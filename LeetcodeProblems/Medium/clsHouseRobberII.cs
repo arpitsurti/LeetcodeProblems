3e84ce3b71ff3e1ccc86bed53e8d2cd120acee79
Arpit S <arpittempacc@gmail.com>
2022-06-08 16:06:52 -0400

    213. House Robber II

---
+++ LeetcodeProblems/Medium/clsHouseRobberII.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Medium
+{
+    /* 213. House Robber II
+     *** REF ***
+     * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+        Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+        Example 1:
+        Input: nums = [2,3,2]
+        Output: 3
+        Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+        
+        Example 2:
+        Input: nums = [1,2,3,1]
+        Output: 4
+        Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+        Total amount you can rob = 1 + 3 = 4.
+        
+        Example 3:
+        Input: nums = [1,2,3]
+        Output: 3
+
+        Constraints:
+        1 <= nums.length <= 100
+        0 <= nums[i] <= 1000
+    */
+    public class clsHouseRobberII
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+            if (nums.Length == 2)
+                return Math.Max(nums[0], nums[1]);
+            return Math.Max(helper(nums.Skip(1).ToArray()), helper(nums.Take(nums.Length - 1).ToArray()));
+        }
+
+        private int helper(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+            if (nums.Length == 2)
+                return Math.Max(nums[0], nums[1]);
+            int[] dp = new int[nums.Length];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+            for (int i = 2; i < nums.Length; i++)
+            {
+                dp[i] = Math.Max(dp[i - 1], nums[i] + dp[i - 2]);
+            }
+            return dp[nums.Length - 1];
+        }
+    }
+}
