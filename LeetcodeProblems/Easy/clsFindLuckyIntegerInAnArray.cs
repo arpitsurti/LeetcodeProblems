e83768e0f9b2af53f5fc2de4bd30e54c20aa0090
Arpit <arpittempacc@gmail.com>
2022-07-25 23:32:42 -0400

    1394. Find Lucky Integer in an Array

---
+++ LeetcodeProblems/Easy/clsFindLuckyIntegerInAnArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems.Easy
+{
+    /*1394. Find Lucky Integer in an Array
+     * Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+        Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+        Example 1:
+        Input: arr = [2,2,3,4]
+        Output: 2
+        Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+
+        Example 2:
+        Input: arr = [1,2,2,3,3,3]
+        Output: 3
+        Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+
+        Example 3:
+        Input: arr = [2,2,2,3,3]
+        Output: -1
+        Explanation: There are no lucky numbers in the array.
+ 
+        Constraints:
+        1 <= arr.length <= 500
+        1 <= arr[i] <= 500
+    */
+    public class clsFindLuckyIntegerInAnArray
+    {
+        public int FindLucky(int[] arr)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!dict.ContainsKey(arr[i]))
+                    dict.Add(arr[i], 0);
+                dict[arr[i]]++;
+            }
+            int max = -1;
+            foreach (var item in dict)
+            {
+                if (item.Key == item.Value)
+                    max = Math.Max(max, item.Key);
+            }
+            return max;
+        }
+    }
+}
