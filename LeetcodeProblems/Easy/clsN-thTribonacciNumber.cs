223b719e86719dc20e78467dac335bc02a5f1f62
Arpit <arpittempacc@gmail.com>
2022-08-03 08:34:13 -0400

    1137. N-th Tribonacci Number (Revised)

--- LeetcodeProblems/Easy/clsN-thTribonacciNumber.cs
+++ LeetcodeProblems/Easy/clsN-thTribonacciNumber.cs
@@ -32,16 +32,16 @@ namespace LeetcodeProblems.Easy
     {
         public int Tribonacci(int n)
         {
-            int[] dp = new int[n + 4];
-            if (n >= 0)
-                dp[0] = 0;
-            if (n >= 1)
-                dp[1] = 1;
-            if (n >= 2)
-                dp[2] = 1;
-            for (int i = 0; i <= n; i++)
+            if (n <= 1)
+                return n;
+            if (n == 2)
+                return 1;
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = dp[2] = 1;
+            for (int i = 3; i < n + 1; i++)
             {
-                dp[i + 3] = dp[i] + dp[i + 1] + dp[i + 2];
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
             }
             return dp[n];
         }
