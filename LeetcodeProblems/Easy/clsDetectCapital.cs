4195f2e597fb64150c7ca416936559b80fc16e12
Arpit <arpittempacc@gmail.com>
2022-10-03 11:38:42 -0400

    520. Detect Capital (Revised)

--- LeetcodeProblems/Easy/clsDetectCapital.cs
+++ LeetcodeProblems/Easy/clsDetectCapital.cs
@@ -30,6 +30,48 @@ namespace LeetcodeProblems.Easy
     {
         public bool DetectCapitalUse(string word)
         {
+            //APPROACH 1
+            /*
+                int upperCount = 0;
+                bool isLowerCase = false;
+                for(int i = 0; i < word.Length; i++)
+                {
+                if(i > 0 && isLowerCase)
+                {
+                    if(char.IsUpper(word[i]))
+                        return false;
+                }
+                if(upperCount > 1 && char.IsLower(word[i]))
+                    return false;
+                if(char.IsUpper(word[i]))
+                    upperCount++;
+                else
+                    isLowerCase = true;
+                }
+                return true;
+            */
+            //APPROACH 2
+            /*
+                if(word.Length == 1)
+                    return true;
+                bool caseOne = false;
+                bool caseTwo = false;
+                bool caseThree = false;
+                if((word[0] >= 'A' && word[0] <= 'Z') && (word[1] >= 'A' && word[1] <= 'Z'))
+                    caseOne = true;
+                else if((word[0] >= 'A' && word[0] <= 'Z') && (word[1] >= 'a' && word[1] <= 'z'))
+                    caseThree = true;
+                else
+                    caseTwo = true;
+                for(int i = 1; i < word.Length; i++)
+                {
+                    if(caseOne && !(word[i] >= 'A' && word[i] <= 'Z'))
+                        return false;
+                    else if((caseTwo || caseThree) && !(word[i] >= 'a' && word[i] <= 'z'))
+                        return false;
+                }
+                return true;
+             */
             int left = 0;
             int right = word.Length;
             while (left < right && word[left] - 'A' < 26)
